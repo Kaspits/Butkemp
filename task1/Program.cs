07e0eacff3c658fe28c66d14ab10a3048ea39ccf
Kaspits <azdanova97@gmail.com>
2023-01-25 10:13:34 +0100

    add 1-2-3 tasks and smt else

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+int size = 1_000_000;
+int m = 500_000;
+int[] array = Enumerable.Range(1, size).Select(item => Random.Shared.Next(10)).ToArray();
+
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+
+Stopwatch sw = new();
+sw.Start();
+int max = 0;
+for (int j = 0; j < m; j++)
+    max += array[j];
+
+int t = max;
+for (int i = 1; i < array.Length - m; i++)
+{
+    t = t - array[i - 1] + array[i + (m - 1)];
+    if (t > max)
+        max = t;
+}
+
+sw.Stop();
+
+Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
+Console.WriteLine(max);
---
+++ task_2/Program.cs
@@ -0,0 +1,36 @@
+﻿// int n = 5;
+// int[] array = new int[n];
+// for (int i = 0; i < n; i++)
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("[" + string.Join(", ", array) + "]");
+// Console.WriteLine(array[3]);
+
+// [4,5,3,1,2]
+// O(n)
+//[1,2,3,4,5] - O(n* log n)
+//((5 + 1)/2) * 5 - O(1)
+// int summa = 0;
+// for (int i = 0; i < n; i++)
+//     summa += array[i];
+
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, n];
+for (int i = 0; i < n; i++)
+{
+    for (int j = i; j < n; j++)
+    {
+        matrix[i, j] = (i + 1) * (j + 1);
+        matrix[j, i] = (i + 1) * (j + 1);
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[i, j]);
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+}
+// O(n^2 / 2)
