24b6989afdaa09c1fcaf53dff2e453be2b880f4e
Kaspits <azdanova97@gmail.com>
2023-01-25 11:57:33 +0100

    организация структуры

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,24 +1,8 @@
 ﻿//Сортировка выбором
-void SortSelection(int[] collection)
-{
-    int size = collection.Length;
+using static Sorting;
+using static Infrastucture;
 
-    for (int i = 0; i < size - 1; i++)
-    {
-        int pos = i;
-        for (int j = i + 1; j < size; j++)
-        {
-            if (collection[j] < collection[pos])
-                pos = j;
-        }
-        int temp = collection[i];
-        collection[i] = collection[pos];
-        collection[pos] = temp;
-    }
-}
-
-int[] array = { 7, 6, 3, 4, 5, 1, 2, 3 };
-
-Console.WriteLine($"[{String.Join(',', array)}]");
+int[] array = CreateArray(10);
+Show(array);
 SortSelection(array);
-Console.WriteLine($"[{String.Join(',', array)}]");
+Show(array);
